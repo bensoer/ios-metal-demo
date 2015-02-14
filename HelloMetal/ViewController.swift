f38b692d61f490e9a170a38779b469325a707bf3
bensoer <ikben@shaw.ca>
2015-02-14 00:34:01 -0800

    Tutorial One Complete. Triangle Draws

--- HelloMetal/ViewController.swift
+++ HelloMetal/ViewController.swift
@@ -5,14 +5,102 @@
 //  Created by Ben Soer on 2015-02-13.
 //  Copyright (c) 2015 bensoer. All rights reserved.
 //
+/* Source: http://www.raywenderlich.com/77488/ios-8-metal-tutorial-swift-getting-started */
+/* Source: http://www.raywenderlich.com/81399/ios-8-metal-tutorial-swift-moving-to-3d */
 
 import UIKit
+import Metal
+import QuartzCore
 
 class ViewController: UIViewController {
 
+    var device: MTLDevice! = nil;
+    
+    var metalLayer: CAMetalLayer! = nil
+    
+    var vertexBuffer: MTLBuffer! = nil
+    
+    let vertexData:[Float] = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0]
+    
+    var pipelineState: MTLRenderPipelineState! = nil
+    
+    var commandQueue: MTLCommandQueue! = nil
+    
+    /* -- END OF SETUP ATTRIBUTES -- */
+    
+    var timer: CADisplayLink! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        device = MTLCreateSystemDefaultDevice();
+        
+        metalLayer = CAMetalLayer()          // 1
+        metalLayer.device = device           // 2
+        metalLayer.pixelFormat = .BGRA8Unorm // 3
+        metalLayer.framebufferOnly = true    // 4
+        metalLayer.frame = view.layer.frame  // 5
+        view.layer.addSublayer(metalLayer)   // 6
+        
+        let dataSize = vertexData.count * sizeofValue(vertexData[0]) // 1
+        vertexBuffer = device.newBufferWithBytes(vertexData, length: dataSize, options: nil) // 2
+        
+        // 1
+        let defaultLibrary = device.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.newFunctionWithName("basic_fragment")
+        let vertexProgram = defaultLibrary!.newFunctionWithName("basic_vertex")
+        
+        // 2
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .BGRA8Unorm //could be an issue
+        
+        // 3
+        var pipelineError : NSError?
+        pipelineState = device.newRenderPipelineStateWithDescriptor(pipelineStateDescriptor, error: &pipelineError)
+        if pipelineState == nil {
+            println("Failed to create pipeline state, error \(pipelineError)")
+        }
+        
+        commandQueue = device.newCommandQueue()
+        
+        /* -- END OF SETUP CODE -- */
+        
+        timer = CADisplayLink(target: self, selector: Selector("gameloop"))
+        timer.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+    }
+    
+    func render() {
+        // TODO
+        var drawable = metalLayer.nextDrawable()
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture //could be an issue
+        renderPassDescriptor.colorAttachments[0].loadAction = .Clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.commandBuffer()
+        
+        let renderEncoderOpt = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+        if let renderEncoder = renderEncoderOpt {
+            renderEncoder.setRenderPipelineState(pipelineState)
+            renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, atIndex: 0)
+            renderEncoder.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+            renderEncoder.endEncoding()
+        }
+        
+        commandBuffer.presentDrawable(drawable)
+        commandBuffer.commit()
+    }
+    
+    func gameloop() {
+        autoreleasepool {
+            self.render()
+        }
     }
 
     override func didReceiveMemoryWarning() {
